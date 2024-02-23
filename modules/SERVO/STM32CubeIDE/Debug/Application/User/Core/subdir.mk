3b3b2e4cd8faf74e1908e4ded95d83ba7b78699b
Human-Gem <NNNhan1882002@gmail.com>
2024-02-23 17:32:18 +0700

    update servo

---
+++ modules/SERVO/STM32CubeIDE/Debug/Application/User/Core/subdir.mk
@@ -0,0 +1,55 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+D:/Micro/Practice_Microprocessor/Practice_Finnal/Autonomous_Driving_Embedded_System/modules/SERVO/Core/Src/gpio.c \
+D:/Micro/Practice_Microprocessor/Practice_Finnal/Autonomous_Driving_Embedded_System/modules/SERVO/Core/Src/main.c \
+D:/Micro/Practice_Microprocessor/Practice_Finnal/Autonomous_Driving_Embedded_System/modules/SERVO/Core/Src/stm32f4xx_hal_msp.c \
+D:/Micro/Practice_Microprocessor/Practice_Finnal/Autonomous_Driving_Embedded_System/modules/SERVO/Core/Src/stm32f4xx_it.c \
+../Application/User/Core/syscalls.c \
+../Application/User/Core/sysmem.c \
+D:/Micro/Practice_Microprocessor/Practice_Finnal/Autonomous_Driving_Embedded_System/modules/SERVO/Core/Src/tim.c 
+
+OBJS += \
+./Application/User/Core/gpio.o \
+./Application/User/Core/main.o \
+./Application/User/Core/stm32f4xx_hal_msp.o \
+./Application/User/Core/stm32f4xx_it.o \
+./Application/User/Core/syscalls.o \
+./Application/User/Core/sysmem.o \
+./Application/User/Core/tim.o 
+
+C_DEPS += \
+./Application/User/Core/gpio.d \
+./Application/User/Core/main.d \
+./Application/User/Core/stm32f4xx_hal_msp.d \
+./Application/User/Core/stm32f4xx_it.d \
+./Application/User/Core/syscalls.d \
+./Application/User/Core/sysmem.d \
+./Application/User/Core/tim.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Application/User/Core/gpio.o: D:/Micro/Practice_Microprocessor/Practice_Finnal/Autonomous_Driving_Embedded_System/modules/SERVO/Core/Src/gpio.c Application/User/Core/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F401xE -c -I../../Core/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Application/User/Core/main.o: D:/Micro/Practice_Microprocessor/Practice_Finnal/Autonomous_Driving_Embedded_System/modules/SERVO/Core/Src/main.c Application/User/Core/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F401xE -c -I../../Core/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Application/User/Core/stm32f4xx_hal_msp.o: D:/Micro/Practice_Microprocessor/Practice_Finnal/Autonomous_Driving_Embedded_System/modules/SERVO/Core/Src/stm32f4xx_hal_msp.c Application/User/Core/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F401xE -c -I../../Core/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Application/User/Core/stm32f4xx_it.o: D:/Micro/Practice_Microprocessor/Practice_Finnal/Autonomous_Driving_Embedded_System/modules/SERVO/Core/Src/stm32f4xx_it.c Application/User/Core/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F401xE -c -I../../Core/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Application/User/Core/%.o Application/User/Core/%.su: ../Application/User/Core/%.c Application/User/Core/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F401xE -c -I../../Core/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Application/User/Core/tim.o: D:/Micro/Practice_Microprocessor/Practice_Finnal/Autonomous_Driving_Embedded_System/modules/SERVO/Core/Src/tim.c Application/User/Core/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F401xE -c -I../../Core/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc -I../../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Application-2f-User-2f-Core
+
+clean-Application-2f-User-2f-Core:
+	-$(RM) ./Application/User/Core/gpio.d ./Application/User/Core/gpio.o ./Application/User/Core/gpio.su ./Application/User/Core/main.d ./Application/User/Core/main.o ./Application/User/Core/main.su ./Application/User/Core/stm32f4xx_hal_msp.d ./Application/User/Core/stm32f4xx_hal_msp.o ./Application/User/Core/stm32f4xx_hal_msp.su ./Application/User/Core/stm32f4xx_it.d ./Application/User/Core/stm32f4xx_it.o ./Application/User/Core/stm32f4xx_it.su ./Application/User/Core/syscalls.d ./Application/User/Core/syscalls.o ./Application/User/Core/syscalls.su ./Application/User/Core/sysmem.d ./Application/User/Core/sysmem.o ./Application/User/Core/sysmem.su ./Application/User/Core/tim.d ./Application/User/Core/tim.o ./Application/User/Core/tim.su
+
+.PHONY: clean-Application-2f-User-2f-Core
+
